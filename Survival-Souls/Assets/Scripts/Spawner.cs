ad8091a700d04c436003451cf5c2002351f88252
Raulincher <93159054+Raulincher@users.noreply.github.com>
2022-11-26 01:34:13 +0100

    Tier spawner type (basic)

--- Survival-Souls/Assets/Scripts/Spawner.cs
+++ Survival-Souls/Assets/Scripts/Spawner.cs
@@ -4,24 +4,63 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
-    public GameObject enemyPrefab;
-
-    public GameObject player;
-
+    public GameObject[] EnemyPrefab;
+    private float[] delays = { 5f, 7f };
+    private int MaxEnemies = 15;
+    private float low_tier_time = 20f;
+    private float medium_tier_time = 60f;
+    private float high_tier_time = 450f;
+    private float boss_tier_time = 600f;
+    private float timeStart;
 
     void Start()
     {
+        timeStart = Time.time;
+        StartCoroutine(spawnEnemy1(EnemyPrefab, delays, timeStart));
+
     }
 
-
-    public void Spawn()
+    private IEnumerator spawnEnemy1(GameObject[] EnemyPrefabs, float[] delay, float timeStart)
     {
 
-        GameObject enemy = GameObject.Instantiate(enemyPrefab);
-        enemy.transform.position = transform.position;
-        //enemy.name = "Enemy";
-        //enemy.GetComponentInChildren<Enemy>().player = player;
+        if (Time.time - timeStart <= low_tier_time)
+        {
+            if (MaxEnemies <= GameObject.FindGameObjectsWithTag("Enemy").Length)
+            {
+                yield return new WaitForSeconds(delay[0]);
+                StartCoroutine(spawnEnemy1(EnemyPrefabs, delay, timeStart));
+            }
+            else
+            {
+                yield return new WaitForSeconds(delay[0]);
+                GameObject enemy = Instantiate(EnemyPrefabs[0]);
+                StartCoroutine(spawnEnemy1(EnemyPrefabs, delay, timeStart));
+            }
+
+        }
+        else
+        {
+            StartCoroutine(spawnEnemy2(EnemyPrefabs, delay, timeStart));
+        }
+
+    }
+
+    private IEnumerator spawnEnemy2(GameObject[] EnemyPrefabs, float[] delay, float timeStart)
+    {
+        if (Time.time - timeStart > low_tier_time && Time.time - timeStart <= medium_tier_time)
+        {
+            if (MaxEnemies <= GameObject.FindGameObjectsWithTag("Enemy").Length)
+            {
+                yield return new WaitForSeconds(delay[1]);
+                StartCoroutine(spawnEnemy2(EnemyPrefabs, delay, timeStart));
+            }
+            else
+            {
+                yield return new WaitForSeconds(delay[1]);
+                GameObject enemy = Instantiate(EnemyPrefabs[1]);
+                StartCoroutine(spawnEnemy2(EnemyPrefabs, delay, timeStart));
+            }
+        }
 
     }
 }
