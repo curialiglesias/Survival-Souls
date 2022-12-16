9553194a3c096a6c82ea4880856b4079a23d12f4
Raulincher <raulincher@gmail.com>
2022-12-16 18:21:23 +0100

    pequeño arreglo

--- Survival-Souls/Assets/Scripts/Spawner.cs
+++ Survival-Souls/Assets/Scripts/Spawner.cs
@@ -8,19 +8,25 @@ public class Spawner : MonoBehaviour
 {
     private float[] delays = { 3f, 7f };
     private int MaxEnemies = 15;
-    private float low_tier_time = 20f;
-    private float medium_tier_time = 60f;
+    private float low_tier_time = 200f;
+    private float medium_tier_time = 300f;
     private float high_tier_time = 450f;
     private float boss_tier_time = 600f;
     private float timeStart;
-
-    Transform maincam = GameObject.Find("MainCamera").GetComponent<Transform>();
+    Vector3 playerPos;
 
 
     void Start()
     {
         timeStart = Time.time;
         StartCoroutine(spawnEnemy1(delays, timeStart));
+    }
+
+
+    private void Update()
+    {
+         playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+
     }
 
     private IEnumerator spawnEnemy1(float[] delay, float timeStart)
@@ -30,18 +36,12 @@ public class Spawner : MonoBehaviour
         {
             if (Time.time - timeStart <= low_tier_time)
             {
-                if (MaxEnemies <= GameObject.FindGameObjectsWithTag("SlimeEnemy").Length)
-                {
-                    yield return new WaitForSeconds(delay[0]);
-                    StartCoroutine(spawnEnemy1(delay, timeStart));
-                }
-                else
-                {
-                    yield return new WaitForSeconds(delay[0]);
-                    slime.transform.position = new Vector3(maincam.position.x - 50, maincam.position.y - 50, 0);
-                    slime.SetActive(true);
-                    StartCoroutine(spawnEnemy1(delay, timeStart));
-                }
+
+                yield return new WaitForSeconds(delay[0]);
+                slime.transform.position = new Vector3(playerPos.x - 1, playerPos.y - 1, 0);
+                slime.SetActive(true);
+                StartCoroutine(spawnEnemy1(delay, timeStart));
+                
             }
             else
             {
@@ -56,18 +56,11 @@ public class Spawner : MonoBehaviour
         if (golem != null) {
             if (Time.time - timeStart > low_tier_time && Time.time - timeStart <= medium_tier_time)
             {
-                if (MaxEnemies <= GameObject.FindGameObjectsWithTag("GolemEnemy").Length)
-                {
-                    yield return new WaitForSeconds(delay[1]);
-                    StartCoroutine(spawnEnemy2(delay, timeStart));
-                }
-                else
-                {
-                    yield return new WaitForSeconds(delay[1]);
-                    golem.transform.position = new Vector3(maincam.position.x - 50, maincam.position.y - 50, 0);
-                    golem.SetActive(true);
-                    StartCoroutine(spawnEnemy2(delay, timeStart));
-                }
+                yield return new WaitForSeconds(delay[1]);
+                golem.transform.position = new Vector3(playerPos.x - 1, playerPos.y - 1, 0);
+                golem.SetActive(true);
+                StartCoroutine(spawnEnemy2(delay, timeStart));
+              
             }
         }
     }
