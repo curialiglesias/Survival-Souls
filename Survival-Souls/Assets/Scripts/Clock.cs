40aaeffad6b9442f3239392adfeada02f19ea5ec
Joan Llobet <j.llobetruiz@gmail.com>
2022-12-23 17:33:53 +0100

    Clock

--- Survival-Souls/Assets/Scripts/Clock.cs
+++ Survival-Souls/Assets/Scripts/Clock.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Clock : MonoBehaviour
 {
 
     public float time = 0;
-    // Update is called once per frame
+    public TextMeshProUGUI clockText;
+
+
+
+
     void Update()
     {
-         time += Time.deltaTime;
+        time += Time.deltaTime;
+        Display();
+    }
+    
+    public void Display()
+    {
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+
+        clockText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
