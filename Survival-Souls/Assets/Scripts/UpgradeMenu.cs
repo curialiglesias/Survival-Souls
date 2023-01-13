132b34315f08799271a5e5beb5865a04a49490f0
Joan Llobet <j.llobetruiz@gmail.com>
2023-01-13 15:19:55 +0100

    Upgrades 2

--- Survival-Souls/Assets/Scripts/UpgradeMenu.cs
+++ Survival-Souls/Assets/Scripts/UpgradeMenu.cs
@@ -1,25 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class UpgradeMenu : MonoBehaviour
 {
-    public TextMeshProUGUI moneyValue;
+    public TextMeshProUGUI moneyText;
+    public int moneyValue;
     PlayerData PlayerData;
-
+    public TextMeshProUGUI upgrade1;
+    public TextMeshProUGUI upgrade2;
+    public TextMeshProUGUI upgrade3;
+    public TextMeshProUGUI upgrade4;
+    public TextMeshProUGUI upgrade5;
     void Start()
     {
-        PlayerData = JSONSaving.SharedInstance.LoadDataFromPath("C:\\Users\\jllob\\AppData\\LocalLow\\StreetLight-Productions\\Survival-Souls\\SaveData.json");
+        PlayerData = JSONSaving.SharedInstance.LoadDataFromPath(Application.persistentDataPath + Path.AltDirectorySeparatorChar + "SaveData.json");
+        moneyValue = PlayerData.points;
     }
 
     public void Display()
     {
-        moneyValue.text = "Points: " + PlayerData.points.ToString();
+        moneyText.text = "Points: " + moneyValue.ToString();
     }
     // Update is called once per frame
     void Update()
     {
         Display();
+    }
+    public void purchase(TextMeshProUGUI upgrade)
+    {
+        int value;
+        int.TryParse(upgrade.text, out value);
+        value++;
+        upgrade.text = value.ToString();
+        moneyValue = moneyValue - 50;
+    }
+    public void Save()
+    {
+
+        int value1;
+        int value2;
+        int value3;
+        int value4;
+        int value5;
+        int.TryParse(upgrade1.text, out value1);
+        int.TryParse(upgrade2.text, out value2);
+        int.TryParse(upgrade3.text, out value3);
+        int.TryParse(upgrade4.text, out value4);
+        int.TryParse(upgrade5.text, out value5);
+        PlayerData.points = moneyValue;
+        PlayerData.velocity = value1;
+        PlayerData.attack = value2;
+        PlayerData.soulTime = value3;
+        PlayerData.decraseRate = value4;
+        PlayerData.bonusPoints = value5;
+
+        JSONSaving.SharedInstance.SaveData(PlayerData);
     }
 }
