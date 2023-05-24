f3eb3ea514bd658694834b59e86a96236cbb01f8
Raulincher <93159054+Raulincher@users.noreply.github.com>
2023-05-24 20:33:35 +0200

    Revert "map 2 spawns"
    This reverts commit ddcde242c9e402360c056720f958d25f2c26b702.

--- Survival-Souls/Assets/Scripts/TimedRoom.cs
+++ Survival-Souls/Assets/Scripts/TimedRoom.cs
@@ -6,111 +6,14 @@ using UnityEngine;
 public class TimedRoom : MonoBehaviour
 {
     public int RoomLight = 0;
-    public float delay = 2f;
-    public float xmax, xmin, ymax, ymin;
-    private float x, y;
-    private int randomSpawn;
-    public static Spawner SharedInstance;
 
 
     void Start()
     {
-        InvokeRepeating("TickLight", 10, 30);
-        StartCoroutine(spawnEnemy(delay));
-
+        InvokeRepeating("TickLight", 10, 60);
     }
 
     // Update is called once per frame
-
-
-
-
-    private void setActiveGolems(GameObject golem, GameObject golemIce, GameObject golemRock)
-    {
-        x = UnityEngine.Random.Range(xmin, xmax);
-        y = UnityEngine.Random.Range(ymin, ymax);
-        randomSpawn = UnityEngine.Random.Range(1, 4);
-
-        if (randomSpawn == 1)
-        {
-            golem.transform.position = new Vector3(x, y, 0);
-            golem.SetActive(true);
-        }
-        else if (randomSpawn == 2)
-        {
-            golemIce.transform.position = new Vector3(x, y, 0);
-            golemIce.SetActive(true);
-        }
-        else
-        {
-            golemRock.transform.position = new Vector3(x, y, 0);
-            golemRock.SetActive(true);
-        }
-    }
-
-
-    private void setActiveSlimes(GameObject slime)
-    {
-        x = UnityEngine.Random.Range(xmin, xmax);
-        y = UnityEngine.Random.Range(ymin, ymax);
-        slime.transform.position = new Vector3(x, y, 0);
-        slime.SetActive(true);
-    }
-
-
-    private IEnumerator spawnEnemy(float delay)
-    {
-        GameObject slime = ObjectPools.SharedInstance.GetPooledObject("SlimeEnemy");
-        GameObject golem = ObjectPools.SharedInstance.GetPooledObject("GolemEnemy");
-        GameObject golemIce = ObjectPools.SharedInstance.GetPooledObject("GolemiceEnemy");
-        GameObject golemRock = ObjectPools.SharedInstance.GetPooledObject("GolemrockEnemy");
-
-        if (slime != null && golem != null && golemIce != null && golemRock != null)
-        {
-
-            if (RoomLight >= 0 && RoomLight < 5)
-            {
-                if (RoomLight >= 0 && RoomLight < 2)
-                {
-                    int randomProbability = UnityEngine.Random.Range(1, 10);
-                    if (randomProbability > 0 && randomProbability < 2)
-                    {
-                        setActiveGolems(golem, golemIce, golemRock);
-                        yield return new WaitForSeconds(2f);
-                        StartCoroutine(spawnEnemy(delay));
-                    }
-                    else
-                    {
-                        setActiveSlimes(slime);
-                        yield return new WaitForSeconds(2f);
-                        StartCoroutine(spawnEnemy(delay));
-                    }
-
-                }
-                else if (RoomLight >= 2 && RoomLight < 5)
-                {
-                    int randomProbability = UnityEngine.Random.Range(1, 10);
-                    if (randomProbability > 0 && randomProbability < 4)
-                    {
-                        setActiveGolems(golem, golemIce, golemRock);
-                        yield return new WaitForSeconds(1.5f);
-                        StartCoroutine(spawnEnemy(delay));
-                    }
-                    else
-                    {
-                        setActiveSlimes(slime);
-                        yield return new WaitForSeconds(1.5f);
-                        StartCoroutine(spawnEnemy(delay));
-                    }
-                }
-            }
-            else
-            {
-                yield return new WaitForSeconds(delay);
-
-            }
-        }        
-    }
 
 
     private void TickLight()
@@ -122,9 +25,6 @@ public class TimedRoom : MonoBehaviour
             CancelInvoke();
         }
     }
-
-
-
 
 
 }
