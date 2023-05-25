86ba57cc32f618c360478f1d9c8644035ff00de9
Raulincher <93159054+Raulincher@users.noreply.github.com>
2023-05-25 23:39:28 +0200

    bug fixes for builds, etc

--- Survival-Souls/Assets/Scripts/TimedRoom.cs
+++ Survival-Souls/Assets/Scripts/TimedRoom.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Presets;
 using UnityEngine;
 
 public class TimedRoom : MonoBehaviour
