6980b214bc9e5022c885f520ad916678c94d58bd
curialiglesias <curialiglesias@gmail.com>
2023-05-26 16:47:45 +0200

    FinalCredits fix

--- Survival-Souls/Assets/Scripts/credits.cs
+++ Survival-Souls/Assets/Scripts/credits.cs
@@ -8,11 +8,18 @@ public class credits : MonoBehaviour
 {
     public GameObject creditsPanel;
     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
         {
-            if (collision.tag == "Player")
-            {
+            GameObject.Find("Music").GetComponent<AudioSource>().Pause();
+            GameObject.Find("Cursor").SetActive(false);
+            collision.GetComponent<PlayerController>().enabled = false;
+            collision.GetComponent<WeaponAim>().enabled = false;
+            collision.GetComponent<ShootBow>().enabled = false;
+            collision.GetComponent<AudioSource>().Pause();
+            Cursor.visible = true;
             creditsPanel.SetActive(true);
-            }
-
         }
+
+    }
 }
